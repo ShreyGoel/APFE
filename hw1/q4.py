5ac2d47549e6468b0df3253334151092e4402bbc
Shrey Goel <shreyg16@gmail.com>
2018-09-23 20:33:45 -0400

    q4 final
    q4 final

--- hw1/q4.py
+++ hw1/q4.py
@@ -5,54 +5,31 @@ Created on Sun Sep 23 16:05:42 2018
 @author: Shrey
 """
 from q2 import GetCov, GetPrices
+from PowerMethod import GetEigens
 import numpy as np
-import copy
+import copy, time
 
 prices = GetPrices()
 
 matrix = GetCov(prices)
 
 # input tolerance
-tol = 0.001
+tol = 0.01
 
 refMatrix = copy.deepcopy(matrix)
 
-def GetEigens(matrix, tol):
-    matrixShape = matrix.shape[0]
-    eigenValues = []
-    eigenVectors = []
-    
-    # repeat until eigenvalue to biggest eigenvalue ratio goes below tolerance
-    for i in range(matrixShape):
-        eigenVectorRandom = np.random.normal(size=(matrixShape))
-        
-        # used for loop so that loop terminates in 100 iterations for sure
-        for k in range(100):
-            
-            if k==0:
-                prevEigenVector = copy.deepcopy(eigenVectorRandom)
-            else:
-                prevEigenVector = copy.deepcopy(eigenVector)
-                matrix = np.matmul(matrix, matrix)
+#time the squared method
+start = time.time()
 
-            eigenVector = np.matmul(matrix, eigenVectorRandom)
-            eigenVector = eigenVector/np.linalg.norm(eigenVector)
-            eigenValue = np.matmul(np.matmul(eigenVector.T, matrix), eigenVector)
-            
-            # break if norm of change in eigenVectors is below threshold
-            if np.linalg.norm(eigenVector - prevEigenVector) <= 0.001:
-                break
-        
-        eigenValues.append(eigenValue)
-        eigenVectors.append(eigenVector)
-        
-        if eigenValue/eigenValues[0] > tol:
-            x = np.outer(eigenVector, eigenVector)
-            matrix = matrix - eigenValue * x
-        else:
-            break
-    
-    return eigenValues, eigenVectors
+matrix_squared = np.linalg.matrix_power(matrix, 2)
+eigenValues, eigenVectors = GetEigens(matrix_squared, tol)
+eigenValues = np.sqrt(eigenValues)
 
-eigenValues, eigenVectors = GetEigens(matrix, tol)
+timer1 = time.time()
+print('squared', timer1 - start)
+
+#time the normal method
+eigenValues1, eigenVectors1 = GetEigens(matrix, tol)
+print('normal', time.time() - timer1)
+
 F, V = np.linalg.eigh(refMatrix)
