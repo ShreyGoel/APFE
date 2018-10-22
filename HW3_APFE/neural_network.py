320fc23eca5bf842c6b91e1c804e94e66df00278
Dutt <Anurag.Dutt@thomsonreuters.com>
2018-10-22 10:42:10 -0400

    Adding function to keep updating parameters

--- HW3_APFE/neural_network.py
+++ HW3_APFE/neural_network.py
@@ -269,6 +269,31 @@ def L_model_backward(AL, output_matrix, caches):
 
 	return grads
 
+def update_parameters(parameters, grads, learning_rate):
+    """
+    Update parameters using gradient descent
+    
+    Arguments:
+    parameters -- python dictionary containing your parameters 
+    grads -- python dictionary containing your gradients, output of L_model_backward
+    
+    Returns:
+    parameters -- python dictionary containing your updated parameters 
+                  parameters["W" + str(l)] = ... 
+                  parameters["b" + str(l)] = ...
+    """
+    
+    L = len(parameters) // 2 # number of layers in the neural network
+
+    # Update rule for each parameter. Use a for loop.
+    ### START CODE HERE ### (≈ 3 lines of code)
+    for l in range(L):
+        parameters["W" + str(l + 1)] = parameters["W" + str(l + 1)] - learning_rate * grads["dW" + str(l + 1)]
+        parameters["b" + str(l + 1)] = parameters["b" + str(l + 1)] - learning_rate * grads["db" + str(l + 1)]
+    ### END CODE HERE ###
+        
+    return parameters
+
 
 if __name__ == "__main__":
 
